609da945ac1cbda515df1c637fe4f13b5eabfc21
MrInfinity <owens1427@gmail.com>
2019-10-06 21:42:23 -0400

    Add files via upload

---
+++ CatAnimator.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sounds : MonoBehaviour
+{
+
+    public AudioSource musicSource;
+
+    public AudioClip musicClipOne;
+
+    public AudioClip musicClipTwo;
+
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            musicSource.clip = musicClipOne;
+            musicSource.Play();
+        }
+
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            musicSource.Stop();
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            musicSource.clip = musicClipTwo;
+            musicSource.Play();
+        }
+
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            musicSource.Stop();
+        }
+    }
+}
